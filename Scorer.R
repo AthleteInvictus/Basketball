c3a4d3792265f41c1abc969e764c852c39338f2f
AthleteInvictus <cantucade@gmail.com>
2016-06-06 16:26:39 -0400

    Fixed bench and volume calculations

--- Scorer.R
+++ Scorer.R
@@ -1,5 +1,5 @@
-mp = 2530
-fg = 567
+mp = 370
+fg = 77
 
 points = matrix(
   c(0,0,240,3000,805,3000,fg,mp),
@@ -12,10 +12,14 @@ fgerror = abs(fg-805)/805
 mperror = abs(mp-2900)/2900
 error = (1-((fgerror+mperror)/2))/.95
 
-volume = abs(mp-2900)/2900
+if((2900-mp)<mp) {
+  volume = mperror
+} else {
+  volume = 1-mperror
+}
 
 plot(points)
 
-cat("Scorer: ",error1)
+cat("Scorer: ",error)
 cat("Pure Volume: ",volume)
-cat("Bench: ",(1-(volume+error)))
+cat("Bench: ",(1-volume))
